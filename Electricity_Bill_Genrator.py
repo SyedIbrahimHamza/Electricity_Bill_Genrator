acdb898476ff4ad64eb127b237cb1f39fffd6357
syedibrahimhamza <ibrahimhamza1211@gmail.com>
2026-09-24 10:49:21 -0700

    Add electricity bill calculation function

--- Electricity_Bill_Genrator.py
+++ Electricity_Bill_Genrator.py
@@ -1,1 +1,12 @@
 bills=[]
+def bill_generator(name,units):
+    if units<=100:
+        bill=units*5
+    elif units<=200:
+        bill=100*5+(units-100)*7
+    elif units<=300:
+        bill=100*5+100*7+(units-200)*10
+    else:
+        bill=100*5+100*7+100*10+(units-300)*15
+    bills.append((name,bill))
+    return bill
